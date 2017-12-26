28f174a59bb5d08efb663bb9279151a3b78ac960
Shivani Mathur <qwellery@gmail.com>
2017-12-26 07:42:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   end
-  elsif int % 5 == 0
+  if int % 5 == 0
     "Buzz"
   end
-  elsif int % 3 == 0 && int % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
   else
